b572597ec6f83229ff75a8640fab857286af7159
Rhian Evans <rhian.evans100@btinternet.com>
2018-11-01 18:20:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ let ageIsValid = Number.isInteger(currentAge);
 
 let randomNumber = (Math.random() * 20) + 1;
 
-let randomInteger = Math.floor(randomNumber)
+let randomInteger = Math.floor(randomNumber);
 
-let randomUserID = randomInteger + 1000000000
+let randomUserID = randomInteger + 1000000000;
